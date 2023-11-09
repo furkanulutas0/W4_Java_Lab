6aacb334ef3971ebc73b5f2f5348366c342bb468
Furkan Ulutaş <furkanulutas@icloud.com>
2023-11-09 21:36:50 +0300

    fix: some error improvement added

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
     	NumberConverterManager numberConverterManager = new NumberConverterManager();
-    	Scanner myScanner = new Scanner(System.in);
-    	System.out.print("Input a number (0-100000): ");
-    	int number = myScanner.nextInt();
-    	
-    	
-        System.out.println(number + "  >>>  " + numberConverterManager.convertToWords(number));
+        
+    	try (Scanner myScanner = new Scanner(System.in)) {
+            System.out.print("Input a number (0-100000): ");
+            int number = myScanner.nextInt();
+            System.out.println(number + "  >>>  " + numberConverterManager.convertToWords(number));
+        }
     }
 }
