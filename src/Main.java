cb9721e3cebdfd37e63523b10606c73716bc4abb
Furkan Ulutaş <furkanulutas@icloud.com>
2023-11-09 21:30:58 +0300

    feat: added number converter functions.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        NumberConverterManager numberConverterManager = new NumberConverterManager();
+
+        System.out.println(numberConverterManager.convertToWords(95633));
     }
 }
--- src/NumberConverterManager.java
+++ src/NumberConverterManager.java
@@ -1,3 +1,35 @@
-﻿public class NumberConverterManager{
-  
+﻿public class NumberConverterManager implements INumberConverterManager {
+
+  private final String[] ones = { "", "Bir", "İki", "Üç", "Dört", "Beş", "Altı", "Yedi", "Sekiz", "Dokuz" };
+  private final String[] tens = { "", "On", "Yirmi", "Otuz", "Kırk", "Elli", "Altmış", "Yetmiş", "Seksen", "Doksan" };
+
+  @Override
+  public String convertToWords(int number) {
+    if (number < 0 || number > 100000) {
+      return "Geçersiz giriş";
+    }
+
+    if (number == 0) {
+      return "Sıfır";
+    }
+    try {
+      return convert(number);
+    } catch (Exception e) {
+      throw new Error(e);
+    }
+  }
+
+  private String convert(int number) {
+    if (number < 10) {
+      return ones[number];
+    } else if (number < 100) {
+      return tens[number / 10] + " " + convert(number % 10);
+    } else if (number < 1000) {
+      return ones[number / 100] + " Yüz " + convert(number % 100);
+    } else if (number < 10000) {
+      return ones[number / 1000] + " Bin " + convert(number % 1000);
+    } else {
+      return tens[number / 10000] + " " + ones[number / 1000 % 10] + " Bin " + convert(number % 1000);
+    }
+  }
 }
