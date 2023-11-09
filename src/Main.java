8980d3a3490dc2d88d4aec28b86534579953e031
Furkan Ulutaş <furkanulutas@icloud.com>
2023-11-09 21:34:53 +0300

    feat:imported Scanner class and added functionality

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        NumberConverterManager numberConverterManager = new NumberConverterManager();
-
-        System.out.println(numberConverterManager.convertToWords(95633));
+    	NumberConverterManager numberConverterManager = new NumberConverterManager();
+    	Scanner myScanner = new Scanner(System.in);
+    	System.out.print("Input a number (0-100000): ");
+    	int number = myScanner.nextInt();
+    	
+    	
+        System.out.println(number + "  >>>  " + numberConverterManager.convertToWords(number));
     }
 }
